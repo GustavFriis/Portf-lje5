b61380f31844f5cb45f648525e3fc18c69d10487
Oskar <lansoskar@gmail.com>
2022-11-16 11:18:56 +0100

    connected + express

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const mysql = require("mysql2");
 require('dotenv').config();
-const port = 3000;
+port = 3000
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(3000)
 
 const mysqlConnection = mysql.createConnection({
     host: process.env.HOST,
@@ -20,3 +29,13 @@ mysqlConnection.connect((err) => {
     }
 });
 
+mysqlConnection.query(
+"SELECT UserName, UserMail FROM cafe_database.users WHERE userID > 5;",
+    (err, results, fields) => {
+        if (!err) {
+            console.log(results);
+        } else {
+            console.log(err);
+            console.log('connection Failed');
+    }
+});
