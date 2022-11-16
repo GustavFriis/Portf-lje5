e735ff43ceb001bd3ecd5c926e43448c04414fb7
Oskar <lansoskar@gmail.com>
2022-11-16 10:58:53 +0100

    connected

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-console.log("hello")
-console.log('oskar test')
+const mysql = require("mysql2");
+require('dotenv').config();
+const port = 3000;
+
+const mysqlConnection = mysql.createConnection({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.MYSQL_USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    multipleStatements: true,
+});
+
+mysqlConnection.connect((err) => {
+    if (!err) {
+        console.log('connected');
+    } else {
+        console.log(err);
+        console.log('connection Failed');
+    }
+});
+
